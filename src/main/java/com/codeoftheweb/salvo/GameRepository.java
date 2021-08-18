585697b9c776d0130bb75765b55dce824684478e
Commit 1 <matu_seba@hotmail.com>
2021-08-18 18:11:33 -0300

    Tarea 4 Finalizada

--- src/main/java/com/codeoftheweb/salvo/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeoftheweb.salvo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
